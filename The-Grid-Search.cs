be7c11cf88c3c65e49ba982ed9e56cf06c459651
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-09-30 18:46:47 +0900

    Create The-Grid-Search.cs

---
+++ The-Grid-Search.cs
@@ -0,0 +1,26 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'gridSearch' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY G
+     *  2. STRING_ARRAY P
+     */
+}
